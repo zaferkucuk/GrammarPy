d273b86b5773702112140a0f20f7a40bdfd5890a
zaferkucuk <53651443+zaferkucuk@users.noreply.github.com>
2022-05-10 12:18:44 +0200

    V1

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #from functions import get_html, get_url
+from tkinter import UNDERLINE
 import streamlit as st
 import requests
 import pandas as pd
@@ -16,40 +17,294 @@ import urllib.request
 import language_tool_python
 from spellchecker import SpellChecker
 import json
+import time
+from annotated_text import annotated_text, annotation
+from PIL import Image
 
-st.set_page_config(page_title='Grammar Checker')
+#------print-------#
+
+
+
+#------print-------#
+logo_small=Image.open("logo2.png")
+st.set_page_config(page_title='GrammarPy', page_icon=logo_small)
 
 ##page=st.sidebar.radio("Tabs", tabs)
 
 ##if page=="input":
+from PIL import Image
+image = Image.open('GrammarPy_Logo1.png')
+
+st.image(image, width=160)
 st.markdown("<h1 style='text-align:center;>Check grammar of your website'</h1>", unsafe_allow_html=True)
 st.write("""Enter website URL you want to check for grammar recommendations""")
 
+
+
 url=st.text_input("URL", "")
-# check_url=st.button('Check URL')
-# #if check_url==True:
-#     #st.write(get_url()) 
+check_url=st.button('Check Grammar')
+if check_url==True:
+         
+        col1, col2, col3, col4 = st.columns(4)
+        with col1:
+            def get_html():
+                #url=get_url()
+                full_url='http://'+url
+                #print(url)
+                #print(full_url)
+                response = requests.get(full_url)    
+                #response.status_code
+                test_html = response.text
+                print(test_html)
+                return test_html
+            get_html()
+            #progress bar
+            my_bar = st.progress(0)
+            for percent_complete in range(100):
+                time.sleep(0.1)
+                my_bar.progress(percent_complete + 1)
 
-tabs=["Extracted Text", "Check Result"]
+        soup = BeautifulSoup(get_html(), 'html.parser')
+        text = soup.get_text(separator=' ')
+        cleaned_text2 = re.sub("([+#^/|*(){}$~`<>=_])|(\[)|(\])", "", text)
+        #cleaned_text2 = re.sub("([-+@#^/|*(){}$~`<>=_])|(\[)|(\])|([0-9])", "", text)
+        #cleaned_text2 = int(text).replace("\n","", text)
+        #cleaned_text2
+                #progress bar
+        my_bar = st.progress(0)
+        for percent_complete in range(100):
+            time.sleep(0.1)
+            my_bar.progress(percent_complete + 1)
+        ##---------original/corrected sentences-------##
+        #st.write("Result:")
+        ## language_tool_python
+        import language_tool_python
+        tool = language_tool_python.LanguageTool('en-US')
+        corrected_text=tool.correct(cleaned_text2)
+        #st.write(corrected_text)
+                #progress bar
+        my_bar = st.progress(0)
+        for percent_complete in range(100):
+            time.sleep(0.1)
+            my_bar.progress(percent_complete + 1)
+        with st.expander("See original text"):
+            st.write(cleaned_text2)
+        st.download_button('Download original text', cleaned_text2, 'text/csv')
+        with st.expander("See corrected text"):
+            st.write(corrected_text)
+        st.download_button('Download corrected text', cleaned_text2, 'text/csv')
+        ##---------original/corrected sentences-------##
 
-def get_html():
-    #url=get_url()
-    full_url='http://'+url
-    #print(url)
-    #print(full_url)
-    response = requests.get(full_url)    
-    #response.status_code
-    test_html = response.text
-    print(test_html)
-    return test_html
-get_html()
+        
+        #st.write("Misspelled words:")
+        words_original = cleaned_text2.split()
+        st.write(words_original)
+
+        words_corrected = corrected_text.split()
+        st.write(words_corrected)
+
+        wrong_words=[]
+        for word in words_original:
+            if word not in words_corrected:
+                wrong_words.append(word)
+            else:
+                pass
+        st.write(wrong_words)
+        
 
 
-soup = BeautifulSoup(get_html(), 'html.parser')
-text = soup.get_text(separator=' ')
-cleaned_text2 = re.sub("([-+@#^/|*(){}$~`<>=_])|(\[)|(\])|([0-9])", "", text)
-#cleaned_text2 = int(text).replace("\n","", text)
-cleaned_text2
+##-------split sentences-----##
+alphabets= "([A-Za-z])"
+prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+websites = "[.](com|net|org|io|gov)"
+
+def split_into_sentences(text):
+    text = " " + text + "  "
+    text = text.replace("\n"," ")
+    text = re.sub(prefixes,"\\1<prd>",text)
+    text = re.sub(websites,"<prd>\\1",text)
+    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
+    text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
+    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
+    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
+    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
+    text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
+    if "”" in text: text = text.replace(".”","”.")
+    if "\"" in text: text = text.replace(".\"","\".")
+    if "!" in text: text = text.replace("!\"","\"!")
+    if "?" in text: text = text.replace("?\"","\"?")
+    text = text.replace(".",".<stop>")
+    text = text.replace("?","?<stop>")
+    text = text.replace("!","!<stop>")
+    text = text.replace("<prd>",".")
+    sentences = text.split("<stop>")
+    sentences = sentences[:-1]
+    sentences = [s.strip() for s in sentences]
+    return sentences
+splitted_sentences_original=split_into_sentences(cleaned_text2)
+#splitted_sentences_original
+
+splitted_sentences_corrected=split_into_sentences(corrected_text)
+#splitted_sentences_corrected
+
+false_words=[]
+for word in splitted_sentences_original:
+    if word not in splitted_sentences_corrected:
+        false_words.append(word)
+false_words  
+
+original_words = cleaned_text2.split()
+corrected_words = corrected_text.split()
+
+wordle = pd.DataFrame(corrected_words)
+wordle.to_csv('file2.csv', index=False, header=False)
+#original_words.to_csv('C:/Users\Guest1\Projects\grammar_checker\wordle_words\"wordle".csv')
+#st.download_button('send words', original_words, 'wordle/csv')
+
+st.write("try")
+underlined_sentence=[]
+for sentence in splitted_sentences_original:
+    if sentence not in splitted_sentences_corrected:
+        underlined_sentence.append((sentence, "#8ef"))
+    else:
+         underlined_sentence.append(sentence)
+
+underlined_words=[]
+for word in words_original:
+    if word not in words_corrected:
+        underlined_words.append((word, "#afa"))
+    else:
+         underlined_words.append(word)
+
+underlined_sentence2=[]
+
+# for sentence in splitted_sentences_original:
+#     if sentence not in splitted_sentences_corrected:
+#         underlined_words2=[]
+#         for i in sentence:
+#             if i not in words_corrected:
+#                 underlined_words2.append((i, "#afa"))
+#             underlined_sentence2.extend((sentence, underlined_words2))
+#     else:
+#          underlined_sentence2.append(sentence)
+
+with st.echo():
+    for sentence in underlined_sentence2:
+        annotated_text(sentence)
+
+
+with st.echo():
+    for sentence in underlined_sentence:
+        annotated_text(sentence)
+
+with st.echo():
+    for word in underlined_words:
+        annotated_text(word)
+
+
+
+# with st_stdout("code"):
+#     print(underlined_text)
+#st.write(underlined_text)
+# output = st.empty()
+# with st_capture(output.code):
+#     print(underlined_text)
+
+for word in original_words:
+    mistaken_words=[]
+    if word not in corrected_words:
+        mistaken_words.append(word)        
+    #mistaken_words = list(filter(None, mistaken_words))
+    #mistaken_words = [s for s in mistaken_words if not mistaken_words == '']
+mistaken_words
+    # for i in mistaken_words:
+    #     if i == '':
+    #         mistaken_words.remove(i)
+    # st.write(mistaken_words)
+
+
+
+# with st.echo():
+#     annotated_text(
+#         "This ",
+#         ("is", "verb", "#8ef"),
+#         " some ",
+#         ("annotated", "adj", "#faa"),
+#         ("text", "noun", "#afa"),
+#         " for those of ",
+#         ("you", "pronoun", "#fea"),
+#         " who ",
+#         ("like", "verb", "#8ef"),
+#         " this sort of ",
+#         ("thing", "noun", "#afa"),
+#         "."
+#     )
+
+
+#st.write("find differences")
+
+# splitted sentences
+# for word in splitted_text_corrected:
+#     corrected_word=[]
+#     if word != splitted_text_original(word):
+#         corrected_word.append(word)
+#     print(f'{word}:{corrected_word}')
+#st.write("sentences")
+
+
+
+
+# with st.echo():
+#     annotated_text(false_words, "#8ef")
+
+
+
+
+# my_bar = st.progress(0)
+# for percent_complete in range(100):
+#     time.sleep(0.1)
+#     my_bar.progress(percent_complete + 1)
+
+# with st.spinner('Wait for it...'):
+#     time.sleep(5)
+# st.success('Done!')
+
+ 
+
+#with st.echo():[false_words, "#faa"]
+    
+
+
+
+
+# st.write('---------')
+
+
+
+
+
+# st.write('---------')
+
+
+## ----------------------------------------------
+# create red underline for the misspelled words:
+# def findErrors(dictionaryWords, cleaned_text2):
+#     misspelledWords=[]
+#     for word in cleaned_text2:
+#         if word not in dictionaryWords:
+#             misspelledWords.append(word)
+#     return misspelledWords
+## ----------------------------------------------
+
+#tabs=["Extracted Text", "Check Result"]
+
+
+
 
 # dir(SpellChecker)
 # def check():
@@ -65,11 +320,3 @@ cleaned_text2
 #             print(f'{word}:{a}')
 #     return check
 # check()
-st.write("Result:")
-## language_tool_python
-import language_tool_python
-test_text="Photo by Gren Chameleon on Unsplash Transfor Learning came as a game changer for all the NLP researchers, so before diving in let’s quickly revisit what transfer learning is. Transfer learning is a machine learning method where a model developed for a task is reused as the starting point for a model on a second task. Simply it leverages prior knowledge from one domain and task into a different domain and task. Luckily for us, we have various such models, which have prior knowledge about the language and its semantics, so we will just use those knowledgeable models and see how they perform on the task we have in handhere checking grammar. "
-tool = language_tool_python.LanguageTool('en-US')
-corrected_text=tool.correct(cleaned_text2)
-#print(corrected_text)
-st.write(corrected_text)
---
+++ puzzle.py
@@ -0,0 +1,198 @@
+import streamlit as st
+
+
+from rich.prompt import Prompt
+from rich.console import Console
+from random import choice
+from words import word_list
+
+SQUARES = {
+    'correct_place': '🟩',
+    'correct_letter': '🟨',
+    'incorrect_letter': '⬛'
+}
+
+WELCOME_MESSAGE = f'\n[white on blue] WELCOME TO WORDLE OF GRAMMARPY [/]\n'
+PLAYER_INSTRUCTIONS = "You may start guessing\n"
+GUESS_STATEMENT = "\nEnter your guess"
+ALLOWED_GUESSES = 6
+
+def correct_place(letter):
+    return f'[black on green]{letter}[/]'
+
+
+def correct_letter(letter):
+    return f'[black on yellow]{letter}[/]'
+
+
+def incorrect_letter(letter):
+    return f'[black on white]{letter}[/]'
+
+
+def check_guess(guess, answer):
+    guessed = []
+    wordle_pattern = []
+    for i, letter in enumerate(guess):
+        if answer[i] == guess[i]:
+            guessed += correct_place(letter)
+            wordle_pattern.append(SQUARES['correct_place'])
+        elif letter in answer:
+            guessed += correct_letter(letter)
+            wordle_pattern.append(SQUARES['correct_letter'])
+        else:
+            guessed += incorrect_letter(letter)
+            wordle_pattern.append(SQUARES['incorrect_letter'])
+    return ''.join(guessed), ''.join(wordle_pattern)
+
+
+def game(console, chosen_word):
+    end_of_game = False
+    already_guessed = []
+    full_wordle_pattern = []
+    all_words_guessed = []
+
+    while not end_of_game:
+        guess = Prompt.ask(GUESS_STATEMENT).upper()
+        while len(guess) != 5 or guess in already_guessed:
+            if guess in already_guessed:
+                console.print("[red]You've already guessed this word!!\n[/]")
+            else:
+                console.print('[red]Please enter a 5-letter word!!\n[/]')
+            guess = Prompt.ask(GUESS_STATEMENT).upper()
+        already_guessed.append(guess)
+        guessed, pattern = check_guess(guess, chosen_word)
+        all_words_guessed.append(guessed)
+        full_wordle_pattern.append(pattern)
+
+        console.print(*all_words_guessed, sep="\n")
+        if guess == chosen_word or len(already_guessed) == ALLOWED_GUESSES:
+            end_of_game = True
+    if len(already_guessed) == ALLOWED_GUESSES and guess != chosen_word:
+        console.print(f"\n[red]WORDLE X/{ALLOWED_GUESSES}[/]")
+        console.print(f'\n[green]Correct Word: {chosen_word}[/]')
+    else:
+        console.print(f"\n[green]WORDLE {len(already_guessed)}/{ALLOWED_GUESSES}[/]\n")
+    console.print(*full_wordle_pattern, sep="\n")
+
+
+if __name__ == '__main__':
+    console = Console()
+    chosen_word = choice(word_list)
+    console.print(WELCOME_MESSAGE)
+    console.print(PLAYER_INSTRUCTIONS)
+    game(console, chosen_word)
+
+
+
+
+# import random
+
+# words = ['programming', 'tiger', 'lamp', 'television',
+# 'laptop', 'water', 'microscope', 'doctor', 'youtube',
+# 'projects']
+
+# random_word = random.choice(words)
+# st.write(random_word)
+# user_guesses = ''
+# chances = 10
+# while chances > 0:
+#     wrong_guesses = 0
+#     for character in random_word:
+#         if character in user_guesses:
+#             st.write(f"Correct guess: {character}")
+#             #print(f"Correct guess: {character}")
+#         else:
+#             wrong_guesses += 1
+#             st.write('_')
+#             #print('_')
+    
+#         if wrong_guesses == 0:
+#             st.write("Correct.")
+#             st.write(f"Word : {random_word}")
+#             #print("Correct.")
+#             #print(f"Word : {random_word}")
+#         break
+#     #guess = input('Make a guess: ')
+#     guess=st.text_input("make a guess")
+#     user_guesses += guess
+
+#     if guess not in random_word:
+#         chances -= 1
+#         st.write(f"Wrong. You have {chances} more chances")
+#         #print(f"Wrong. You have {chances} more chances")
+
+#         if chances == 0:
+#             st.write('game over')
+#             #print('game over')
+
+
+
+
+
+
+
+
+
+
+
+
+# import string 
+# import random 
+
+
+# width = 8
+# height = 10 
+
+
+
+# def place_word(word, grid):
+#     word = random.choice([word, word[::-1]])
+    
+#     direction = random.choice([[1,0], [0,1], [1,1]])
+#     print(f'Placing {word} in direction {direction}...')
+#     xstart = width if direction[0] == 0 else width - len(word) - 1
+#     ystart = height if direction[1] == 0 else height - len(word) - 1
+
+#     x = random.randrange(0, xstart)
+#     y = random.randrange(0, ystart)
+
+#     print([x, y])
+
+#     for c in range(len(word)):
+#         grid[x + direction[0]*c][y + direction[1]*c] = word[c]
+#     return grid
+
+# grid = [[random.choice(string.ascii_uppercase) for i in range(width)] 
+#         for j in range(height)]
+
+# for word in ["HELLO","lotr","NINE","EIGHT"]:
+#     place_word(word, grid)
+
+
+
+# print("\n".join(map(lambda row: " ".join(row), grid)))
+
+# """Example output:
+
+# Placing OLLEH in direction [1, 1]...
+# [0, 3]
+# Placing DOOR in direction [0, 1]...
+# [0, 3]
+# Placing ENIN in direction [1, 0]...
+# [0, 0]
+# Placing THGIE in direction [1, 1]...
+# [1, 2]
+# e N L d o o r N\n
+# n T t B l J U Q\n
+# i Z G h L l D Q\n
+# n K S Y g M e D\n
+# Q V O I H i C h\n
+# K U W H K L e X\n
+# A A V M K X W N\n
+# G X D Q U E S B\n
+# W C G R E P R J\n
+# N G P V Q X N W\n
+# """
+
+
+# st.write("\n".join(map(lambda row: " ".join(row), grid)))
